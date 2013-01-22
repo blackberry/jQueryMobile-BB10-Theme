418b33e5bd5656d3896b731f05ede8f5300a890c
Jason Scott <jasscott@rim.com>
2013-01-22 12:26:21 -0500

    Actionbar: Adding pressed state to tab button on vmousedown

--- src/plugins/actionbar/jquery.mobile.actionbar.js
+++ src/plugins/actionbar/jquery.mobile.actionbar.js
@@ -114,7 +114,7 @@
 			itemsize +=  "-" + ( ( itemsLen > 5 ) ? "e" : String.fromCharCode( 96 + itemsLen ) );
 			actionBarItems.addClass(itemsize);
 
-			$( '.action-bar-action-item' )
+			$( '.action-bar-action-item, .action-bar-tab-item' )
 				.bind( 'vmousedown', function( event ) {
 					$(event.delegateTarget).addClass( 'pressed' );
 				})
